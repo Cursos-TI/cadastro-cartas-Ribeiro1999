bc87d21d13ab6ad5b8416e9c8fcaac4ca9046f98
Ribeiro1999 <alisonhr63@gmail.com>
2026-01-08 01:18:20 +0000

    criei as duas cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,7 +5,27 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+  //CARTA Nº1
+  char estado1 ='A'; //definindo variavel char para o caractere referente ao estado escolhido.
+  char codigo1[5] = "A1"; //variavel char para definir o codigo da carta escolhida.
+  char nome[20] = "Parana"; //definindo variavel char com array para strings com nome do estado escolhido.
+  int população = 11890; // variavel int possui um valores inteiros, assim foi definido a populacao do estado escolhido.
+  float area = 199.300; // variavel float possui valores com casas decimais, assim como foi definido a area referente a metros quadrados do estado.
+  float PIB = 670.9; // mesma variavel da area, tambem foi usada para definir o PIB do estado.
+  int turismo = 2333; // mesma variavel usada para definir a papulacao com valores inteiros, foi usada aqui para definir o numero de pontos turisticos 
+
+
+
+//CARTA Nº2
+  char estado2 = 'B'; 
+  char codigo2[5] = "A2"; 
+  char nome2[20] = "bahia"; 
+  int população2 = 23059; 
+  float area2 = 567.295; 
+  float PIB2 = 430.9; 
+  int turismo2 = 4365;
+
+
 
   // Área para entrada de dados
 
